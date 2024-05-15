ec7bec6c7a71716abc12f6567a8bcf638bb494fd
Lior Ruslander <liorruslander@gmail.com>
2024-05-15 21:58:40 +0300

    add SQS

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 from flask import Flask, request, jsonify 
+import boto3
+import json
+import os
 app = Flask(__name__)
 server_token="notsecured"   # todo: change it to get from env
-
+sqs_client = boto3.client("sqs",
+    region_name="us-east-1",#os.environ.get('AWS_DEFAULT_REGION'),
+    endpoint_url="http://localhost:4566",#os.environ.get('AWS_ENDPOINT'),
+    aws_access_key_id="my_access_key", #os.environ.get('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key="my_secret_key")#os.environ.get('AWS_SECRET_ACCESS_KEY'))
+queue_url = sqs_client.get_queue_url(
+        QueueName="checkpoint-SQS",
+    )["QueueUrl"]
 @app.route('/api/add_message', methods=['POST'])
 def add_message():
     content = request.json
     content_valid=validate_content(content)
     if(validate_content(content)!=""):
         return content_valid,400
+    send_message_to_sqs(content)
     return "",200
+
+def send_message_to_sqs(content):
+    message = content
+    response = sqs_client.send_message(
+        QueueUrl=queue_url,
+        MessageBody=json.dumps(message)
+    )
+    print(response)
+
+
+
+
 
 def validate_content(content):
         try: 
@@ -48,8 +71,12 @@ def validate_content(content):
         
         return ""
         
+
         
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0',debug=True)
+
+    
+    
    
